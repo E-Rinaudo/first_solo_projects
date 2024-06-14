e16c1086488607489a8a1f4716c039ded3146182
enrico_rinaudo <enricorinaudo91@gmail.com>
2024-06-14 15:59:04 +0200

    Add the first data visualization project.

---
+++ solo_projects/data_visualization/molecular_motion/molecular_motion.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+from molecular_motion_random_walk import MolecularMotion
+
+# Flag to generate multiple walks.
+random_walk = True
+
+while random_walk:
+    # Generate the random walk.
+    mm = MolecularMotion(50000)
+    mm.make_walk()
+
+    # Make the plot.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(14, 5.5), dpi=130)
+    num_points = range(mm.num_points)
+    ax.scatter(mm.x_values, mm.y_values, c=num_points, cmap=plt.cm.Reds, 
+        edgecolors='none', s=1)
+    ax.set_aspect('equal')
+    ax.set_title("Pollen Grain Walk", fontsize=14)
+
+    # Emphasize the start and end points.
+    ax.scatter(0, 0, color='green', label='Start Walk', edgecolors='none', s=80)
+    ax.scatter(mm.x_values[-1], mm.y_values[-1], color='blue', label='End Walk',
+        edgecolors='none', s=80)
+
+    # Make a legend.
+    ax.legend(loc='upper left', scatterpoints=1, fancybox=True, shadow=True, 
+        fontsize=8)
+
+    # Remove the axes.
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+
+    plt.show()
+
+    # Make a new walk.
+    new_walk = input("\nMake another walk? (y/n) ")
+
+    if new_walk != 'y':
+        random_walk = False
---
+++ solo_projects/data_visualization/molecular_motion/molecular_motion_random_walk.py
@@ -0,0 +1,35 @@
+from random import choice
+
+
+class MolecularMotion:
+    """A class for random walk of a pollen grain on a drop of water."""
+
+    def __init__(self, num_points=5000):
+        """Initiliaze the random walk attributes."""
+        self.num_points = num_points
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def make_walk(self):
+        """Generate the random walk."""
+        while len(self.x_values) < self.num_points:
+            x_step = self._get_step()
+            y_step = self._get_step()
+
+            # Discard steps that go nowhere.
+            if x_step == 0 and y_step == 0:
+                continue
+
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+    
+    def _get_step(self):
+        """Get the direction and distance of the steps."""
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5])
+        step = direction * distance
+
+        return step
