2e6482cef428af92ff7ea0b0766917302a154acb
enrico_rinaudo <enricorinaudo91@gmail.com>
2024-06-19 14:02:43 +0200

    Refactor molecular motion.

--- solo_projects/data_visualization/molecular_motion/molecular_motion_visual.py
+++ solo_projects/data_visualization/molecular_motion/molecular_motion_visual.py
@@ -3,11 +3,19 @@ import matplotlib.pyplot as plt
 from molecular_motion_random_walk import MolecularMotion
 
 
+NUM_POINTS = 50_000
+FIG_SIZE = (14, 5.5)
+DPI = 130
+FONT_SIZE_TITLE = 14
+FONT_SIZE_MAIN_POINTS = 80
+FONT_SIZE_LEGEND = 8 
+
+
 class MolecularVisual:
-    """A class to generate the chart of the pollen grain walk."""
+    """A class to generate a random walk chart of a pollen grain."""
 
     def __init__(self):
-        """Initialize and generate random walks."""
+        """Initialize and generate the random walk."""
         self._random_walk_loop()
 
     def _random_walk_loop(self):
@@ -15,7 +23,7 @@ class MolecularVisual:
         random_walk = True
 
         while random_walk:
-            self.mm = MolecularMotion(num_points=50000)
+            self.mm = MolecularMotion(num_points=NUM_POINTS)
             self.mm.make_walk()
             self._make_plot()
 
@@ -28,30 +36,41 @@ class MolecularVisual:
     def _make_plot(self):
         """Create and display the plot."""
         plt.style.use('classic')
-        fig, ax = plt.subplots(figsize=(14, 5.5), dpi=130)
+        fig, ax = plt.subplots(figsize=FIG_SIZE, dpi=DPI)
+    
+        self._customize_chart(ax)
+        self._make_start_end_points(ax)
+        self._make_legend(ax)
 
+        plt.show()
+
+    def _customize_chart(self, ax):
+        """Customize the random walk."""
         # Number of points used to set the color of each point in the walk.
-        num_points = range(self.mm.num_points)
+        num_points = range(NUM_POINTS)
+
         ax.scatter(self.mm.x_values, self.mm.y_values, c=num_points, 
-            cmap=plt.cm.Reds, edgecolors='none', s=1)
+                   cmap=plt.cm.Reds, edgecolors='none', s=1)
         ax.set_aspect('equal')
-        ax.set_title("Pollen Grain Walk", fontsize=14)
-
-        # Emphasize the start and end points.
-        ax.scatter(0, 0, color='green', label='Start Walk', edgecolors='none', 
-            s=80)
-        ax.scatter(self.mm.x_values[-1], self.mm.y_values[-1], color='blue', 
-                   label='End Walk', edgecolors='none', s=80)
-
-        # Make a legend indicating the start and end points.
-        ax.legend(loc='upper left', scatterpoints=1, fancybox=True, shadow=True, 
-            fontsize=8)
+        ax.set_title("Pollen Grain Walk", fontsize=FONT_SIZE_TITLE)
 
         # Remove the axes for a clearer visualization.
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
 
-        plt.show()
+    def _make_start_end_points(self, ax):
+        """Emphasize the start and end points."""
+        ax.scatter(0, 0, color='green', label='Start Walk', edgecolors='none', 
+                   s=FONT_SIZE_MAIN_POINTS)
+        ax.scatter(self.mm.x_values[-1], self.mm.y_values[-1], color='blue', 
+                   label='End Walk', edgecolors='none', 
+                   s=FONT_SIZE_MAIN_POINTS)
+    
+    def _make_legend(self, ax):
+        """Make a legend indicating the start and end points."""
+        ax.legend(loc='upper left', scatterpoints=1, fancybox=True, shadow=True, 
+                  fontsize=FONT_SIZE_LEGEND)   
+
 
 if __name__ == '__main__':
     # Make the instance and generate the walk.
