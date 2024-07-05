0e9651d5b8e7270dec8f63823091b4fb3e24fc23
enrico_rinaudo <enricorinaudo91@gmail.com>
2024-07-05 10:44:45 +0200

    Refactor repetition_days.

---
+++ solo_projects/repetition_days/pop_up.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from datetime import datetime
+
+
+class StudyPopUp:
+    """Make a pop-up window to display the subject being studied."""
+
+    def __init__(self, days, months, study_method, subject):
+        """Initialize the pop-up attributes."""
+        self.days = days
+        self.months = months
+        self.study_method = study_method
+        self.subject = subject
+
+        # Get the current date in a neatly formatted string.
+        self._get_date()
+
+        # Generate the pop-up.
+        self._make_popup()
+
+    def _make_popup(self):
+        """Generate a pop-up window displaying the current date,
+        the subject being studied, and the tasks for the day.
+        """
+        popup = tk.Tk()
+        self._customize_popup(popup)
+        popup.mainloop()
+
+    def _customize_popup(self, popup):
+        """Customize the pop-up."""
+        popup.title("Spaced Repetition Reminder")
+
+        # Main label.
+        tk.Label(popup, text=f"{self.formatted_date}").pack()
+
+        # Additional label for book/subject title.
+        tk.Label(popup, text=f"{self.subject.title()}:", foreground="dark red").pack()
+
+        self._repetition_text(popup)
+
+    def _repetition_text(self, popup):
+        """Display the text to describe the repetition for the current day."""
+        tasks = self.study_method.get(self.days[self.day_of_week])
+        for i, task in enumerate(tasks[:4]):
+            tk.Label(popup, text=f"Today's task {i + 1}:", foreground="dark blue").pack(
+                anchor="w"
+            )
+            tk.Label(popup, text=f"- {task}.").pack(anchor="w")
+
+    def _get_date(self):
+        """Get and neatly format the current date."""
+        current_date = datetime.now().date()
+
+        # Get the current day and month as int.
+        self.day_of_week = current_date.weekday()
+        month = current_date.month - 1
+
+        # Use the int to get the name of the days and months from days and months lists.
+        day_name = self.days[self.day_of_week].title()
+        month_name = self.months[month].title()
+
+        # Format the date as a string.
+        self.formatted_date = current_date.strftime(f"{day_name}, %d {month_name} %Y")
--- solo_projects/repetition_days/study_method.py
+++ solo_projects/repetition_days/study_method.py
@@ -1,12 +1,12 @@
-# Make a spaced repetion pop up to generate what to study/repeat each day.
-# For convenience, create an alias in Bash to run the script every day.
+# Call the spaced repetion pop-up class to generate what to study/repeat each day.
+# For convenience, create an alias in the terminal to run the script every day.
 
-import tkinter as tk
-from datetime import datetime
 
-days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+from pop_up import StudyPopUp
 
-months = [
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+MONTHS = [
     "january",
     "february",
     "march",
@@ -21,9 +21,7 @@ months = [
     "december",
 ]
 
-# Create a study method for one subject.
-# Generalize it so it works with any subject.
-study_method = {
+STUDY_METHOD = {
     "monday": ["Study new block", "Repeat today's block"],
     "tuesday": ["Repeat yesterday's block", "Study new block", "Repeat today's block"],
     "wednesday": [
@@ -42,67 +40,8 @@ study_method = {
     "sunday": ["Repeat all blocks"],
 }
 
-subject1 = "python crash course"
+subject = "python crash course"
 
-
-# Function to display the pop-up message.
-def show_popup():
-    """
-    Generate a pop-up window displaying the current date,
-    the subject being studied, and the tasks for the day.
-
-    The pop-up window includes:
-    - The current date in the format: "Day Month Year".
-    - The title of the subject being studied.
-    - Tasks scheduled for the current day based on the study method.
-
-    Study tasks are determined based on the current day of the week
-    and defined in the 'study_method' dictionary. Each day's tasks
-    are listed according to the 'study_method' schedule.
-
-    The pop-up window remains open until the user closes it.
-    """
-    # Get the current date.
-    current_date = datetime.now().date()
-    # Format the date as a string.
-    formatted_date_day = current_date.strftime("%d")
-    formatted_date_year = current_date.strftime("%Y")
-    # Get the day of the week and month.
-    day_of_week = current_date.weekday()
-    month = current_date.month - 1
-    # Convert the numeric day and moth to a string.
-    day_name = days[day_of_week]
-    month_name = months[month]
-    # Generate the pop-up.
-    popup = tk.Tk()
-    popup.title("Spaced Repetition Reminder")
-    # Create the main label.
-    label = tk.Label(
-        popup,
-        text=f"{day_name.title()} "
-        f"{formatted_date_day} "
-        f"{month_name.title()} "
-        f"{formatted_date_year}",
-    )
-    label.pack()
-    # Additional label for book/subject title.
-    subject = tk.Label(popup, text=f"{subject1.title()}:", foreground="dark red")
-    subject.pack()
-    # Text for each day study/repetition.
-    for day, task in study_method.items():
-        for i in range(4):
-            if day_name == day:
-                if i > len(task) - 1:
-                    break
-                repetition_title = tk.Label(
-                    popup, text=f"Today's task {i + 1}:", foreground="dark blue"
-                )
-                repetition_title.pack(anchor="w")
-
-                repetition = tk.Label(popup, text=f"- {task[i]}.")
-                repetition.pack(anchor="w")
-
-    popup.mainloop()
-
-
-show_popup()
+if __name__ == "__main__":
+    # Make the instance and generate the pop-up.
+    show_popup = StudyPopUp(DAYS, MONTHS, STUDY_METHOD, subject)
