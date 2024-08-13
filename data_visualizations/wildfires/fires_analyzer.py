80bf546f76251b9586b119f1374ea63aa3519f00
enrico_rinaudo <enricorinaudo91@gmail.com>
2024-08-13 17:51:22 +0200

    Add type annotations and correct a few errors after linting.

--- data_visualizations/wildfires/fires_analyzer.py
+++ data_visualizations/wildfires/fires_analyzer.py
@@ -10,61 +10,28 @@ and brightness using Plotly.
 """
 
 from datetime import datetime
-import pandas as pd
 
+import pandas as pd
 import plotly.graph_objects as go
 
 
 class WildfirePlotter:
     """A class to visualize wildfire activity in North America."""
 
-    def __init__(self, path: str):
+    def __init__(self, path: str) -> None:
         """Initialize the class attributes, read the csv file and plot the data."""
         self.path = path
-        self.acq_times = []
-        self.acq_dates = []
+        self.acq_times: list[str] = []
+        self.acq_dates: list[str] = []
+        self.fires_data: pd.DataFrame = pd.DataFrame()
         self._read_file()
         self._visualize_plot()
 
-    def _read_file(self):
+    def _read_file(self) -> None:
         """Read the csv file."""
         self.fires_data = pd.read_csv(self.path)
 
-    def _format_date(self):
-        """Format the data acquisition date to neatly display it."""
-        df_dates = list(self.fires_data["acq_date"])
-
-        for df_date in df_dates:
-            date_str = str(df_date)
-            datetime_date = datetime.strptime(date_str, "%Y-%m-%d")
-            formatted_date = datetime_date.strftime("%B %d, %Y")
-            self.acq_dates.append(formatted_date)
-
-    def _format_time(self):
-        """Format the data acquisition time to neatly display it."""
-        df_times = list(self.fires_data["acq_time"])
-
-        for df_time in df_times:
-            time_str = str(df_time).zfill(4)
-            datetime_time = datetime.strptime(time_str, "%H%M")
-            formatted_time = datetime_time.strftime("%H:%M (24 HR Format)")
-            self.acq_times.append(formatted_time)
-
-    def _format_label_text(self):
-        """Combine the date, time and brightness into a text for the plot label."""
-        self._format_date()
-        self._format_time()
-
-        self.fires_data["text"] = [
-            f"Acquisition Date: {date} -- {time} -- Brightness: {brightness}"
-            for date, time, brightness in zip(
-                self.acq_dates,
-                self.acq_times,
-                self.fires_data["brightness"].astype(str),
-            )
-        ]
-
-    def _visualize_plot(self):
+    def _visualize_plot(self) -> None:
         """Visualize wildfire activity."""
         self._format_label_text()
         # Lower the brightness value to use it as a size in the plot.
@@ -90,7 +57,41 @@ class WildfirePlotter:
         self._update_plot(fig)
         fig.show(renderer="browser")
 
-    def _update_plot(self, fig: go.Figure):
+    def _format_label_text(self) -> None:
+        """Combine the date, time and brightness into a text for the plot label."""
+        self._format_date()
+        self._format_time()
+
+        self.fires_data["text"] = [
+            f"Acquisition Date: {date} -- {time} -- Brightness: {brightness}"
+            for date, time, brightness in zip(
+                self.acq_dates,
+                self.acq_times,
+                self.fires_data["brightness"].astype(str),
+            )
+        ]
+
+    def _format_date(self) -> None:
+        """Format the data acquisition date to neatly display it."""
+        df_dates = list(self.fires_data["acq_date"])
+
+        for df_date in df_dates:
+            date_str = str(df_date)
+            datetime_date = datetime.strptime(date_str, "%Y-%m-%d")
+            formatted_date = datetime_date.strftime("%B %d, %Y")
+            self.acq_dates.append(formatted_date)
+
+    def _format_time(self) -> None:
+        """Format the data acquisition time to neatly display it."""
+        df_times = list(self.fires_data["acq_time"])
+
+        for df_time in df_times:
+            time_str = str(df_time).zfill(4)
+            datetime_time = datetime.strptime(time_str, "%H%M")
+            formatted_time = datetime_time.strftime("%H:%M (24 HR Format)")
+            self.acq_times.append(formatted_time)
+
+    def _update_plot(self, fig: go.Figure) -> None:
         """Customize the plot."""
         title = "USA Contiguous and Hawaii Wildfire Activity "
         title += f"({self.acq_dates[0]} to {self.acq_dates[-1]})"
