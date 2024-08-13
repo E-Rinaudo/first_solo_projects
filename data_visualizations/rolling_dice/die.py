fd5f604f3369092a10762cf75a6ff8ebced345e6
enrico_rinaudo <enricorinaudo91@gmail.com>
2024-08-13 18:47:13 +0200

    Add type annotations and correct a few errors after linting.

--- data_visualizations/rolling_dice/die.py
+++ data_visualizations/rolling_dice/die.py
@@ -11,10 +11,10 @@ from random import randint
 class Die:
     """A class to represent a die."""
 
-    def __init__(self, num_sides=6):
+    def __init__(self, num_sides: int = 6) -> None:
         """Initialize the die attributes."""
         self.num_sides = num_sides
 
-    def roll(self):
+    def roll(self) -> int:
         """Return the result of the roll of a die."""
         return randint(1, self.num_sides)
--- data_visualizations/rolling_dice/matplotlib_die_visual.py
+++ data_visualizations/rolling_dice/matplotlib_die_visual.py
@@ -8,6 +8,8 @@ The results are displayed in a bar graph using matplotlib.pyplot.
 """
 
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.container import BarContainer
 
 from die import Die
 
@@ -25,19 +27,32 @@ FONT_SIZE_BARS_LABELS = 8
 class DieVisual:
     """A class to visualize the rolls of two D6 dice 50_000 times."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Make the die instance and generate the visualization."""
         # Make two D6.
-        self.die_1 = Die()
-        self.die_2 = Die()
+        self.die_1: Die = Die()
+        self.die_2: Die = Die()
 
-        self._make_plot()
+    def make_plot(self) -> None:
+        """Make a bar chart of the results of the rolls."""
+        poss_results, frequencies = self._analyze_rolls()
 
-    def _dice_rolls(self):
-        """Return the results of the two D6 rolled 50,000 times."""
-        return [self.die_1.roll() + self.die_2.roll() for _ in range(NUM_ROLLS)]
+        # Make the chart.
+        plt.style.use("seaborn-v0_8-muted")
+        fig, ax = plt.subplots(figsize=FIG_SIZE, dpi=DPI)
+        bars = ax.bar(
+            x=poss_results,
+            height=frequencies,
+            color=BARS_COLOR,
+            tick_label=poss_results,
+        )
 
-    def _analyze_rolls(self):
+        self._label_bars(ax, bars)
+        self._chart_customization(ax)
+
+        plt.show()
+
+    def _analyze_rolls(self) -> tuple[range, list[int]]:
         """Return the possible results of the roll and their frequency."""
         results = self._dice_rolls()
 
@@ -46,7 +61,11 @@ class DieVisual:
 
         return poss_results, [results.count(value) for value in poss_results]
 
-    def _label_bars(self, ax, bars):
+    def _dice_rolls(self) -> list[int]:
+        """Return the results of the two D6 rolled 50,000 times."""
+        return [self.die_1.roll() + self.die_2.roll() for _ in range(NUM_ROLLS)]
+
+    def _label_bars(self, ax: Axes, bars: BarContainer) -> None:
         """Add text labels on top of the bars."""
         for bar_ in bars:
             rolls_result = bar_.get_height()
@@ -54,13 +73,13 @@ class DieVisual:
                 ax.text(
                     x=(bar_.get_x() + bar_.get_width() / 2),
                     y=rolls_result,
-                    s=int(rolls_result),
+                    s=str(rolls_result),
                     ha="center",
                     va="bottom",
                     fontsize=FONT_SIZE_BARS_LABELS,
                 )
 
-    def _chart_customization(self, ax):
+    def _chart_customization(self, ax: Axes) -> None:
         """Customize the chart."""
         ax.set_title(
             "Results of Rolling Two D6 Dice 50,000 Times",
@@ -81,26 +100,8 @@ class DieVisual:
         ax.set_xticks(range(2, 13))
         ax.tick_params(labelsize=FONT_SIZE_TICKS)
 
-    def _make_plot(self):
-        """Make a bar chart of the results of the rolls."""
-        poss_results, frequencies = self._analyze_rolls()
-
-        # Make the chart.
-        plt.style.use("seaborn-v0_8-muted")
-        fig, ax = plt.subplots(figsize=FIG_SIZE, dpi=DPI)
-        bars = ax.bar(
-            x=poss_results,
-            height=frequencies,
-            color=BARS_COLOR,
-            tick_label=poss_results,
-        )
-
-        self._label_bars(ax, bars)
-        self._chart_customization(ax)
-
-        plt.show()
-
 
 if __name__ == "__main__":
     # Make the instance to generate the chart.
     dv = DieVisual()
+    dv.make_plot()
